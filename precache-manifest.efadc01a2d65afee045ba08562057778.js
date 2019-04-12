9aa61ece9ee7b94e21455fbe4938b9f183df843b
Vidy <vidy.thatte@gmail.com>
2019-04-11 21:40:51 -0700

    Updates

---
+++ precache-manifest.efadc01a2d65afee045ba08562057778.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8f7f799833b849940a0",
+    "url": "/image-validator-tool/static/js/runtime~main.1ec11cae.js"
+  },
+  {
+    "revision": "ee78dcf682e8b0927600",
+    "url": "/image-validator-tool/static/js/main.1a369354.chunk.js"
+  },
+  {
+    "revision": "de7cae22f1bba50475b5",
+    "url": "/image-validator-tool/static/js/2.95780f71.chunk.js"
+  },
+  {
+    "revision": "ee78dcf682e8b0927600",
+    "url": "/image-validator-tool/static/css/main.b84bf5d2.chunk.css"
+  },
+  {
+    "revision": "db9132d97a760223e5f459d0c1ca6899",
+    "url": "/image-validator-tool/index.html"
+  }
+];
